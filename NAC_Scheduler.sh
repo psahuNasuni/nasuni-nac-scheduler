1088b86ba96b0ca3c6e2f5efb4efe5b7fbeac5cc
Pradeepta Kumar Sahu <psahu@nasuni.com>
2021-12-06 22:35:24 +0530

    4th and 5th arguments

--- NAC_Scheduler.sh
+++ NAC_Scheduler.sh
@@ -5,6 +5,43 @@
 ## 		- Git, aws CLI, JQ 					##
 ##		- AWS Profile Setup as nasuni		##
 ##############################################
+DATE_WITH_TIME=`date "+%Y%m%d-%H%M%S"`	
+START=$(date +%s)
+
+append_nac_keys_values_to_tfvars() {
+  inputFile="$1"   ### Read InputFile
+  outFile="$2"
+  echo "inputFile ::: $inputFile"
+  echo "outFile ::: $outFile"
+  # echo " " >> $outFile
+  while IFS="=" read -r key value; do
+  	echo "$key ::: $value "
+    if [ ${#key} -ne 0 ]; then
+    	echo "$key=$value" >> $outFile
+    fi
+  done < "$inputFile"
+}
+
+check_if_secret_exists(){
+USER_SECRET="$1"
+AWS_PROFILE="$2"
+AWS_REGION="$3"
+	# Verify the Secret Exists 
+	if [[ -n $USER_SECRET ]]; then
+		# echo "USER SECRET:    $USER_SECRET"
+		COMMAND="aws secretsmanager get-secret-value --secret-id ${USER_SECRET} --profile ${AWS_PROFILE} --region ${AWS_REGION}"
+		$COMMAND
+		RES=$?
+		if [[ $RES -eq 0 ]]; then
+			echo "INFO ::: Secret ${USER_SECRET} Exists. $?"
+			echo "Y"
+		else
+			echo "ERROR ::: $? :: Secret ${USER_SECRET} Does'nt Exist in ${AWS_REGION} region. OR, Invalid Secret name passed as 4th parameter"
+			echo "N"
+			# exit 0
+		fi
+	fi
+}
 
 validate_kvp()
 {
@@ -38,10 +75,7 @@ validate_secret_values() {
 		SECRET_VALUE=$(aws secretsmanager get-secret-value --secret-id "$SECRET_NAME" --region "${AWS_REGION}"  --profile "${AWS_PROFILE}" | jq -r '.SecretString' | jq -r '.destination_bucket')
 	elif [ "$SECRET_KEY" == "volume_key" ];then
 		SECRET_VALUE=$(aws secretsmanager get-secret-value --secret-id "$SECRET_NAME" --region "${AWS_REGION}"  --profile "${AWS_PROFILE}" | jq -r '.SecretString' | jq -r '.volume_key')
-	# elif [ "$SECRET_KEY" == "volume_keyZ" ];then   ## Testing Negative flow
-	# 	SECRET_VALUE=$(aws secretsmanager get-secret-value --secret-id "$SECRET_NAME" --region "${AWS_REGION}"  --profile "${AWS_PROFILE}" | jq -r '.SecretString' | jq -r '.volume_keyZ')
 	fi
-	# echo "SECRET_VALUE :::::::::::::::::::::::::: $SECRET_VALUE"
 	if [ -n "$SECRET_VALUE" ]; then
 		echo "SEC ::: $SECRET_NAME ::: $SECRET_KEY ::: $SECRET_VALUE"
 	else   # NOT WORKING - It should go to else part when  SECRET_VALUE is null
@@ -49,9 +83,9 @@ validate_secret_values() {
 		echo "ERROR ::: Invalid value for Key $SECRET_KEY."
 		exit 1
 	fi
-
 }
-parse_textfile_for_keys_values() {
+
+parse_textfile_for_user_secret_keys_values() {
   file="$1"
   while IFS="=" read -r key value; do
     case "$key" in
@@ -66,10 +100,28 @@ parse_textfile_for_keys_values() {
     esac
   done < "$file"
 }
+create_JSON_from_Input_user_KVPfile(){
+file_name=$1
+last_line=$(wc -l < $file_name)
+current_line=0
+echo "{"
+while read line
+do
+  current_line=$(($current_line + 1))
+  if [[ $current_line -ne $last_line ]]; then
+  [ -z "$line" ] && continue
+    echo $line|awk -F'='  '{ print " \""$1"\" : \""$2"\","}'|grep -iv '\"#'
+  else
+    echo $line|awk -F'='  '{ print " \""$1"\" : \""$2"\""}'|grep -iv '\"#'
+  fi
+done < $file_name
+echo "}"
+}
 AWS_PROFILE="nasuni"
 AWS_REGION=""
 AWS_ACCESS_KEY_ID=""
 AWS_SECRET_ACCESS_KEY=""
+ARG_COUNT="$#"
 ######################## Validating AWS profile for NAC ####################################
 validate_aws_profile() {
 	echo "INFO ::: Validating AWS profile for NAC  . . . . . . . . . . . . . . . . !!!"
@@ -93,7 +145,7 @@ validate_aws_profile() {
 #######################################################################################
 
 if [ $# -eq 0 ]; then
-    echo "ERROR ::: No arguments supplied. This Script Takes 4 Mandatory Arguments 1) NMC Volume_Name, 2) Service, 3) Frequency and 4) User Secret(either Existing Secret Name Or Secret KVPs in a text file)"
+    echo "ERROR ::: No argument(s) supplied. This Script Takes 4 Mandatory Arguments 1) NMC Volume_Name, 2) Service, 3) Frequency and 4) User Secret(either Existing Secret Name Or Secret KVPs in a text file)"
 	exit 1
 elif [ $# -lt 4 ]; then
     echo "ERROR ::: $# argument(s) supplied. This Script Takes 4 Mandatory Arguments 1) NMC Volume_Name, 2) Service, 3) Frequency and 4) User Secret(either Existing Secret Name Or Secret KVPs in a text file)"
@@ -104,6 +156,7 @@ NMC_VOLUME_NAME="$1"  		### 1st argument  ::: NMC_VOLUME_NAME
 ANALYTICS_SERVICE="$2"		### 2nd argument  ::: ANALYTICS_SERVICE
 FREQUENCY="$3"     			### 3rd argument  ::: FREQUENCY
 FOURTH_ARG="$4"  			### 4th argument  ::: User Secret a KVP file Or an existing Secret
+NAC_INPUT_KVP="$5"			### 5th argument  ::: User defined KVP file for passing arguments to NAC 
 echo "INFO ::: Validating Arguments Passed to NAC_Scheduler.sh"
 if [ "${#NMC_VOLUME_NAME}" -lt 3 ]
 then 
@@ -128,67 +181,66 @@ if [[ -n "$FOURTH_ARG" ]]; then
 		echo "INFO ::: Fourth argument is a File ${FOURTH_ARG}"
 
         # Parse the user data - KVP
-        parse_textfile_for_keys_values "$FOURTH_ARG"
+        parse_textfile_for_user_secret_keys_values "$FOURTH_ARG"
 		#### Validate the user data file and the provided values 
 		echo "INFO ::: Validating the user data file ${FOURTH_ARG} and the provided values" 
 		validate_kvp nmc_api_username "${NMC_API_USERNAME}" 
 		validate_kvp nmc_api_password "${NMC_API_PASSWORD}"
 		validate_kvp nac_product_key "${NAC_PRODUCT_KEY}"
-		# validate_kvp nac_product_key "${NAC_PRODUCT_KEY}"
 		validate_kvp nmc_api_endpoint "${NMC_API_ENDPOINT}"
 		validate_kvp web_access_appliance_address "${WEB_ACCESS_APPLIANCE_ADDRESS}"
-		# validate_kvp	volume_key "${VOLUME_KEY}"
-		# validate_kvp	volume_key_passphrase "${VOLUME_KEY_PASSPHRASE}"
 		validate_kvp destination_bucket "${DESTINATION_BUCKET}"
 		
-JSON_STRING=$(cat <<EOF
-{
-"nmc_api_username":"$NMC_API_USERNAME",
-"nmc_api_password":"$NMC_API_PASSWORD",
-"nac_product_key":"$NAC_PRODUCT_KEY",
-"nmc_api_endpoint":"$NMC_API_ENDPOINT",
-"web_access_appliance_address":
-"$WEB_ACCESS_APPLIANCE_ADDRESS",
-"volume_key":"$VOLUME_KEY",
-"volume_key_passphrase":"$VOLUME_KEY_PASSPHRASE",
-"destination_bucket":"$DESTINATION_BUCKET"
-}
- 
-EOF
-)
-		# Create JSON Scring for User specific data
-		echo "$JSON_STRING" | tr -d '\n' | tr -d '\r' > user_creds_"${NMC_VOLUME_NAME}"_"${ANALYTICS_SERVICE}".json
-
-
-		# Create user Secret
+		create_JSON_from_Input_user_KVPfile $FOURTH_ARG > user_creds_"${NMC_VOLUME_NAME}"_"${ANALYTICS_SERVICE}".json
+		
+		# Formation of User Secret Name 
 		USER_SECRET="prod/nac/admin/$NMC_VOLUME_NAME/$ANALYTICS_SERVICE"
-		aws secretsmanager create-secret --name "${USER_SECRET}" \
-		--description "Preserving User specific data/secrets to be used for NAC Scheduling" \
-		--secret-string file://user_creds_"${NMC_VOLUME_NAME}"_"${ANALYTICS_SERVICE}".json 
-		rm -rf user_creds_"${NMC_VOLUME_NAME}"_"${ANALYTICS_SERVICE}".json
-	else  ####  Fourth Argument $FOURTH_ARG is passed as User Secret Name
+		# Verify the Secret Exists
+		USER_SECRET_EXISTS=$(check_if_secret_exists $USER_SECRET $AWS_PROFILE $AWS_REGION)
+		echo "INFO ::: START Update Secret"
+		echo "INFO ::: USER_SECRET_EXISTS ::: $USER_SECRET_EXISTS "
+		if [ "$USER_SECRET_EXISTS" != "N" ]; then
+			echo "INFO ::: Fourth argument is a File && the User Secret exists ==> User wants to Update the Secret Values"
+			# Update Secret
+			echo "INFO ::: Update Secret $USER_SECRET "
+			# Update Secret
+			aws secretsmanager update-secret --secret-id "${USER_SECRET}" \
+			--secret-string file://user_creds_"${NMC_VOLUME_NAME}"_"${ANALYTICS_SERVICE}".json \
+			--region "${AWS_REGION}"
+			RES="$?"
+			if [ $RES -ne 0 ]; then
+				echo "INFO ::: $RES Failed to Update Secret $USER_SECRET."
+				exit 1
+			elif [ $RES -eq 0 ]; then
+				echo "INFO ::: Secret $USER_SECRET Updated SUCCESSFULLY"
+			fi
+		else  
+		## Fourth argument is a File && the User Secret Doesn't exist ==> User wants to Create a new Secret 
+			# Create Secret
+			echo "INFO ::: Create Secret $USER_SECRET ######################"
+			aws secretsmanager create-secret --name "${USER_SECRET}" \
+			--description "Preserving User specific data/secrets to be used for NAC Scheduling" \
+			--secret-string file://user_creds_"${NMC_VOLUME_NAME}"_"${ANALYTICS_SERVICE}".json \
+			--region "${AWS_REGION}"
+			RES="$?"
+			if [ $RES -ne 0 ]; then
+				echo "INFO ::: $RES Failed to Create Secret $USER_SECRET as, its already exists."
+				exit 1
+			elif [ $RES -eq 0 ]; then
+				echo "INFO ::: Secret $USER_SECRET Created"
+			fi
+		fi
+		rm -rf *"${NMC_VOLUME_NAME}"_"${ANALYTICS_SERVICE}".json
+	else  ####  Fourth Argument is passed as User Secret Name
 		echo "INFO ::: Fourth Argument $FOURTH_ARG is passed as User Secret Name"
 		USER_SECRET="$FOURTH_ARG"
 		
-		# Verify the Secret Exists 
-		if [[ -n $USER_SECRET ]]; then
-			echo "USER SECRET:    $USER_SECRET"
-			COMMAND="aws secretsmanager get-secret-value --secret-id ${USER_SECRET} --profile ${AWS_PROFILE} --region ${AWS_REGION}"
-			$COMMAND
-			if [[ $? -eq 0 ]]; then
-				echo "INFO ::: Secret ${USER_SECRET} Exists. $?"
-				USER_SECRET_EXISTS="Y"
-			else
-				echo "ERROR ::: $? :: Secret ${USER_SECRET} Does'nt Exist. OR, Invalid Secret name passed as 4th parameter"
-				exit 0
-			fi
-		fi
-		# exit 0
+		# Verify the Secret Exists
+		USER_SECRET_EXISTS=$(check_if_secret_exists $USER_SECRET ${AWS_PROFILE} ${AWS_REGION})
+
 		if [ "$USER_SECRET_EXISTS" == "Y" ]; then
 			# Validate Keys in the Secret 
 			echo  "INFO ::: Check if all Keys are provided"
-			#  SUMEET :: TBD :::::: :: Check if all Keys/Values are provided
-
 			validate_secret_values "$USER_SECRET" nmc_api_username "$AWS_REGION" "$AWS_PROFILE" 
 			validate_secret_values "$USER_SECRET" nmc_api_password "$AWS_REGION" "$AWS_PROFILE" 
 			validate_secret_values "$USER_SECRET" nac_product_key "$AWS_REGION" "$AWS_PROFILE" 
@@ -196,29 +248,16 @@ EOF
 			validate_secret_values "$USER_SECRET" web_access_appliance_address "$AWS_REGION" "$AWS_PROFILE" 
 			validate_secret_values "$USER_SECRET" destination_bucket "$AWS_REGION" "$AWS_PROFILE" 
 			validate_secret_values "$USER_SECRET" volume_key "$AWS_REGION" "$AWS_PROFILE" 
-			# validate_secret_values "$USER_SECRET" volume_keyZ "$AWS_REGION" "$AWS_PROFILE" 
-			# validate_secret_values "$USER_SECRET" volume_keyZ "$AWS_REGION" "$AWS_PROFILE" 
-			# echo "HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH"
-			# exit 1
-			# nmc_api_username	
-			# nmc_api_password	
-			# nac_product_key	
-			# nmc_api_endpoint	
-			# web_access_appliance_address	
-			# destination_bucket
-			# volume_key
 		fi
 	fi
 else 
 	echo "INFO ::: Fourth argument is NOT provided, So, It will consider prod/nac/admin as the default user secret."
 fi
 
-# exit 1
-
+echo "INFO ::: Get IP Address of NAC Scheduler Instance"
 ######################  NAC Scheduler Instance is Available ##############################
 PUB_IP_ADDR=$(aws ec2 describe-instances --query "Reservations[*].Instances[*].{Name:Tags[?Key=='Name']|[0].Value,Status:State.Name,PublicIP:PublicIpAddress}" --filters "Name=tag:Name,Values='NACScheduler'" "Name=instance-state-name,Values=running" --region "${AWS_REGION}" | grep -e "PublicIP" |cut -d":" -f 2|tr -d '"'|tr -d ' ') 
-echo "PUB_IP_ADDR ::: ${PUB_IP_ADDR}"
-
+echo "INFO ::: PUB_IP_ADDR ::: ${PUB_IP_ADDR}"
 
 if [ "$PUB_IP_ADDR" != "" ];then 
 	echo "INFO ::: NAC Scheduler Instance is Available. IP Address: $PUB_IP_ADDR"
@@ -240,19 +279,27 @@ if [ "$PUB_IP_ADDR" != "" ];then
 	echo "region="\"$AWS_REGION\" >> $TFVARS_FILE_NAME
 	echo "volume_name="\"$NMC_VOLUME_NAME\" >> $TFVARS_FILE_NAME
 	echo "user_secret="\"$USER_SECRET\" >> $TFVARS_FILE_NAME
-
+	if [ $ARG_COUNT -eq 5 ]; then
+    	echo "INFO ::: $ARG_COUNT th Argument is supplied as ::: $NAC_INPUT_KVP" 
+		append_nac_keys_values_to_tfvars $NAC_INPUT_KVP $TFVARS_FILE_NAME
+	fi
 	### Create Directory for each Volume 
-	# ssh -i "$PEM" ubuntu@"$PUB_IP_ADDR" -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null "[ ! -d $NMC_VOLUME_NAME ] && mkdir $NMC_VOLUME_NAME "
 	ssh -i "$PEM" ubuntu@"$PUB_IP_ADDR" -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null "[ ! -d $CRON_DIR_NAME ] && mkdir $CRON_DIR_NAME "
-	echo "11111111  Test message"
+	# echo "11111111  Test message"
 	### Copy TFVARS and provision_nac.sh to NACScheduler
 	scp -i "$PEM" -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null provision_nac.sh "$TFVARS_FILE_NAME" ubuntu@$PUB_IP_ADDR:~/$CRON_DIR_NAME
-	
+	RES="$?"
+	if [ $RES -ne 0 ]; then
+		echo "ERROR ::: Failed to Copy $TFVARS_FILE_NAME to NAC_Scheduer Instance."
+		exit 1
+	elif [ $RES -eq 0 ]; then
+		echo "INFO ::: $TFVARS_FILE_NAME Uploaded Successfully to NAC_Scheduer Instance."
+	fi
+	rm -rf $TFVARS_FILE_NAME
 	#dos2unix command execute
 	ssh -i "$PEM" ubuntu@"$PUB_IP_ADDR" -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null "dos2unix ~/$CRON_DIR_NAME/provision_nac.sh"
 	### Check If CRON JOB is running for a specific VOLUME_NAME
 	CRON_VOL=$(ssh -i "$PEM" -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null ubuntu@"$PUB_IP_ADDR" "crontab -l |grep /home/ubuntu/$CRON_DIR_NAME/$TFVARS_FILE_NAME")
-	#*/2 * * * * sh /home/ubuntu/file.sh SA-ES-VOL
 	if [ "$CRON_VOL" != "" ]
 	then
 		### DO Nothing. CRON JOB takes care of NAC Provisioning
@@ -261,8 +308,7 @@ if [ "$PUB_IP_ADDR" != "" ];then
 		### Set up a new CRON JOB for NAC Provisioning
 
 		echo "INFO ::: Setting CRON JOB for $CRON_DIR_NAME as it is not present"
-			
-		ssh -i "$PEM" ubuntu@$PUB_IP_ADDR -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null "(crontab -l ; echo '*/45 * * * * sh ~/$CRON_DIR_NAME/provision_nac.sh  ~/$CRON_DIR_NAME/$TFVARS_FILE_NAME >> ~/$CRON_DIR_NAME/cronlog.log') | sort - | uniq - | crontab -"
+		ssh -i "$PEM" ubuntu@$PUB_IP_ADDR -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null "(crontab -l ; echo '*/$FREQUENCY * * * * sh ~/$CRON_DIR_NAME/provision_nac.sh  ~/$CRON_DIR_NAME/$TFVARS_FILE_NAME >> ~/$CRON_DIR_NAME/CRON_log-$CRON_DIR_NAME-$DATE_WITH_TIME.log') | sort - | uniq - | crontab -"
 		if [ $? -eq 0 ]; then
 			echo "INFO ::: CRON JOB Scheduled for NMC VOLUME and Service :: $CRON_DIR_NAME"
 			exit 0
@@ -271,8 +317,6 @@ if [ "$PUB_IP_ADDR" != "" ];then
 			exit 1
 		fi
 	fi
-	
-exit 1
 ###################### NAC Scheduler EC2 Instance is NOT Available ##############################
 else 
 	## "NAC Scheduler is not present. Creating new EC2 machine."
@@ -327,30 +371,11 @@ else
 	ssh -i "$1" ubuntu@$ip 'sh install_req_pkgs.sh'
 fi
 
+END=$(date +%s)
+secs=$((END - START))
+DIFF=$(printf '%02dh:%02dm:%02ds\n' $((secs/3600)) $((secs%3600/60)) $((secs%60)))
+echo "INFO ::: Total execution Time ::: $DIFF"
+#exit 0
 
 
-Download_git_code(){
-	echo "INFO ::: Start - Git Clone "
-    ### Download Provisioning Code from GitHub
-    GIT_REPO="$1"
-    GIT_REPO_NAME=$(echo ${GIT_REPO} | sed 's/.*\/\([^ ]*\/[^.]*\).*/\1/' | cut -d "/" -f 2)
-    echo "INFO ::: GIT_REPO $GIT_REPO"
-    echo "INFO ::: GIT_REPO_NAME $GIT_REPO_NAME"
-    pwd
-    ls
-    rm -rf "${GIT_REPO_NAME}"
-    COMMAND="git clone -b main ${GIT_REPO}"
-    $COMMAND
-    RESULT=$?
-    if [ $RESULT -eq 0 ]; then
-        echo "INFO ::: git clone SUCCESS"
-        cd "${GIT_REPO_NAME}"
-    elif [ $RESULT -eq 128 ]; then    
-        cd "${GIT_REPO_NAME}"
-        COMMAND="git pull origin main"
-        $COMMAND
-	fi
-
-
-}
 
