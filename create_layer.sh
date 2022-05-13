f4d51ef488af7ba9e475422a97e13b678708d080
Pradeepta Kumar Sahu <psahu@nasuni.com>
2022-05-13 17:28:57 +0530

    Lambda Layer changes

--- create_layer.sh
+++ create_layer.sh
@@ -1,6 +1,6 @@
-#./creater_layer.sh <package_name> <layer_name>
-#./creater_layer.sh elasticsearch my-layer
-#./creater_layer.sh my-layer nasuni
+####./creater_layer.sh <package_name> <layer_name>
+####./creater_layer.sh elasticsearch my-layer
+####./creater_layer.sh my-layer nasuni
 path="app"
 LAYER_NAME="$1"
 AWS_PROFILE="$2"
@@ -19,27 +19,24 @@ echo "INFO ::: EXISTING_LAMBDA_LAYER ::: $EXISTING_LAMBDA_LAYER"
 EXT_LAMBDA_LAYER=$(echo $EXISTING_LAMBDA_LAYER | jq -r '.Layers[] | select(.LayerName == '\"$LAMBDA_LAYER_NAME\"') | {LayerName}')
 echo "ext_lambda_layer $EXT_LAMBDA_LAYER"
 
-if [ "$EXT_LAMBDA_LAYER" = "" ] ; then
-   mkdir -p $path
-   for i in opensearch-py requests requests_aws4auth python-pptx PyMuPDF python-docx pandas chardet openpyxl xlrd
-   do
-     # pip3 install "${package}" --target "${path}/python/lib/python3.9/site-packages/"
-     pip3 install "$i" --target "${path}/python/lib/python3.8/site-packages/"
-     #echo "$i"
-   done
-   cd $path && zip -r ../lambdalayer.zip .
-   aws s3 mb s3://"$LAMBDA_LAYER_NAME" --profile $AWS_PROFILE
+if [ "$EXT_LAMBDA_LAYER" = "" ]; then
+    mkdir -p $path
+    for i in opensearch-py requests requests_aws4auth python-pptx PyMuPDF python-docx pandas chardet openpyxl xlrd; do
+        pip3 install "$i" --target "${path}/python/lib/python3.8/site-packages/"
+    done
+    cd $path && zip -r ../lambdalayer.zip .
+    aws s3 mb s3://"$LAMBDA_LAYER_NAME" --profile $AWS_PROFILE
     if [ $? -eq 0 ]; then
         echo OK
     else
         echo bucket is already exist
     fi
-    #going to add /home/ubuntu directory
+    ###going to add /home/ubuntu directory
     cd
 
     aws s3 cp lambdalayer.zip s3://"${LAMBDA_LAYER_NAME}" --profile $AWS_PROFILE
 
-    aws lambda publish-layer-version --layer-name "${LAMBDA_LAYER_NAME}" --description "Lambda layer for including all pkgs for NAC_Discovery"     --license-info "MIT" --content S3Bucket="${LAMBDA_LAYER_NAME}",S3Key=lambdalayer.zip  --compatible-runtimes python3.8 python3.9 --profile $AWS_PROFILE
+    aws lambda publish-layer-version --layer-name "${LAMBDA_LAYER_NAME}" --description "Lambda layer for including all pkgs for NAC_Discovery" --license-info "MIT" --content S3Bucket="${LAMBDA_LAYER_NAME}",S3Key=lambdalayer.zip --compatible-runtimes python3.8 python3.9 --profile $AWS_PROFILE
 else
-          echo "The layer "${LAMBDA_LAYER_NAME}" is already present"
+    echo "The layer "${LAMBDA_LAYER_NAME}" is already present"
 fi
