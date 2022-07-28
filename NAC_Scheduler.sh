7956d2bd9c0775e3479ef5b943ac948a47f25cd1
Pradeepta Kumar Sahu <psahu@nasuni.com>
2022-07-28 14:13:51 +0530

    test 1

--- NAC_Scheduler.sh
+++ NAC_Scheduler.sh
@@ -134,6 +134,7 @@ check_if_opensearch_exists(){
 			echo "user_vpc_id="\"$USER_VPC_ID\" >>$OS_TFVARS
 			echo "use_private_ip="\"$USE_PRIVATE_IP\" >>$OS_TFVARS
 			echo "es_region="\"$AWS_REGION\" >>$OS_TFVARS
+			echo "git_branch="\"$GIT_BRANCH\" >>$OS_TFVARS
 			echo "" >>$OS_TFVARS
 			echo "INFO ::: TFVARS $OS_TFVARS File created for OpenSearch Provisioning"
 			echo "INFO ::: Amazon_OpenSearch_Service provisioning ::: BEGIN ::: Executing ::: Terraform apply . . . . . . . . . . . . . . . . . . ."
@@ -800,7 +801,7 @@ else
 	rm -rf "${GIT_REPO_NAME}"
 	COMMAND="git clone -b $GIT_BRANCH ${GIT_REPO}"
 	$COMMAND
-	exit 88888
+	# exit 88888
 	RESULT=$?
 	if [ $RESULT -eq 0 ]; then
 		echo "INFO ::: git clone SUCCESS for repo ::: $GIT_REPO_NAME"
